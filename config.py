e180cb59fe5ea053a466482a26d21f6dbb43d84d
WayK17 <109327502+WayK17@users.noreply.github.com>
2025-05-21 14:10:02 -0500

    Actualizar config.py

--- config.py
+++ config.py
@@ -98,12 +98,12 @@ FORCE_SUB_ENABLED = is_enabled(environ.get('FORCE_SUB_ENABLED', "True"), True)
 
 # ID numérico o @username del canal al que deben unirse. ¡El bot DEBE ser admin aquí!
 # Ejemplo: FORCE_SUB_CHANNEL = -10012345678**  o  FORCE_SUB_CHANNEL = "MiCanal"
-FORCE_SUB_CHANNEL = environ.get('FORCE_SUB_CHANNEL', "-1002173951862")
+FORCE_SUB_CHANNEL = environ.get('FORCE_SUB_CHANNEL', "-1002661251789")
 
 # Enlace de invitación del canal (si es privado o quieres usar uno específico)
 # Ejemplo: FORCE_SUB_INVITE_LINK = "https://t.me/joinchat/ABCDEFGHIJKL12345"
 # Si el canal es público, puedes poner el enlace normal (e.g., "https://t.me/MiCanal")
-FORCE_SUB_INVITE_LINK = environ.get('FORCE_SUB_INVITE_LINK', "https://t.me/NessCloud")
+FORCE_SUB_INVITE_LINK = environ.get('FORCE_SUB_INVITE_LINK', "https://t.me/+0mbRE6ULxftkZjNh")
 
 # Opcional: Permitir que los admins del bot salten la verificación (True o False)
 SKIP_FORCE_SUB_FOR_ADMINS = is_enabled(environ.get('SKIP_FORCE_SUB_FOR_ADMINS', "True"), True)
