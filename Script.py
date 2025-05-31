400bcc73fcc392f98a3c03849bcdb2cc0c36d343
WayK17 <109327502+WayK17@users.noreply.github.com>
2025-05-31 10:39:28 -0500

    Actualizar Script.py

--- Script.py
+++ Script.py
@@ -15,7 +15,7 @@ class script(object):
 
 ⚙️ Tamaño: {file_size}
 
-[「Nᴇss Cʟᴏᴜᴅ」](https://t.me/NessCloud)</b>""" 
+[「NESS Cloud」](https://t.me/Ness_Cloud)</b>""" 
 
 # No Eliminar Crédito Tg - @VJ_Botz
 # Suscríbete al Canal de YouTube Para Bots Increíbles https://youtube.com/@Tech_VJ
