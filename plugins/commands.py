4c4b81c22f30828410b3f3b770b4ef7cc10c8f3f
WayK17 <109327502+WayK17@users.noreply.github.com>
2025-05-31 10:40:37 -0500

    Actualizar commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -60,7 +60,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('Únete a Nuestro Canal', url='https://t.me/NessCloud')
+            InlineKeyboardButton('Únete a Nuestro Canal', url='https://t.me/Ness_Cloud')
             ],[
             InlineKeyboardButton('⚠️ Grupo de Soporte', url='https://t.me/NESS_Soporte')
             ]]
